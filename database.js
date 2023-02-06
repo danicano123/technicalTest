384f3229099b3dc2c2c0be80897f2ffb5fdcb751
Daniel Steven Cano Zapata <daniprog2000@gmail.com>
2023-02-06 13:56:11 -0500

    REFACTOR: (databases) changes in env variables

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
-const db = require('mongoose');
-const config = require('./config');
+const db = require("mongoose");
+const config = require("./config");
 
 db.Promise = global.Promise;
 
@@ -7,5 +7,5 @@ db.connect(config.mongoUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-  .then(() => console.log('[MongoDB in cloud]: successfully connected'))
+  .then(() => console.log("[MongoDB in cloud]: successfully connected"))
   .catch((err) => console.log(err));
--- libs/postgres.js
+++ libs/postgres.js
@@ -2,15 +2,15 @@
  * I use the "pg" library to make a connection between node and postgres
  *  that allows me to use query language inside js.
  */
-
+const config = require("../config");
 const { Client } = require("pg");
 async function getConnection() {
   const client = new Client({
     host: "localhost",
     port: 5432,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
+    user: config.PostgresUser,
+    password: config.postgresPassword,
+    database: config.postgresDatabase,
   });
   await client.connect()
   return client;
