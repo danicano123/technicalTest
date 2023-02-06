31947211c6af08e8731dd51215e1ec72983f057c
Daniel Steven Cano Zapata <daniprog2000@gmail.com>
2023-02-06 14:00:16 -0500

    FIX: (health.route.js) env.var problem was fixed

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 const config = {
   mongoUrl: process.env.MONGO_URL || "mongodb://localhost:27017/Users",
-  dbUrl: "mongodb+srv://danicano:Danicano3264-@testdb.c5iv0kv.mongodb.net/test",
   PostgresUser: process.env.POSTGRES_USER,
   postgresPassword: process.env.POSTGRES_PASSWORD,
   postgresDatabase: process.env.POSTGRES_DB,
--- routes/health.route.js
+++ routes/health.route.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const config = require("../config");
 
 const router = express.Router();
 
 // Mongo Connection URL
 const MongoClient = require("mongodb").MongoClient;
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost:27017/Users";
+const mongoUrl = config.mongoUrl || "mongodb://localhost:27017/Users";
 
 router.get("/", async (req, res, next) => {
   try {
